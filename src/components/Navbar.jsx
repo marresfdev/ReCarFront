9bcf4c56e2b28cdbf942c2420cfd9fa006308740
marresf10 <fonseca34689@gmail.com>
2025-03-11 10:31:49 -0600

    navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/Navbar.css'; // Archivo de estilos mejorado
-import logo from "../assets/recarlogo.png"; // Ruta al logo de la empresa
+import '../styles/Navbar.css';
+import logo from "../assets/recarlogo.png";
 import { FaHome, FaCar, FaEnvelope, FaCalculator } from "react-icons/fa";
 
 const Navbar = () => {
   const location = useLocation();
-
+  
   useEffect(() => {
     if (location.hash) {
       const section = document.querySelector(location.hash);
@@ -17,16 +17,17 @@ const Navbar = () => {
     }
   }, [location]);
 
+  // Verificar si la ruta actual es AutoDetalle
+  const isAutoDetalle = location.pathname.startsWith("/auto/");
+
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
-        {/* Logo y Marca */}
         <Link className="navbar-brand" to="/">
           <img src={logo} alt="Logo" className="navbar-logo" />
           <span className="brand-text">ENLAZANDO TUS CAMINOS</span>
         </Link>
 
-        {/* Botón de menú responsive */}
         <button
           className="navbar-toggler custom-toggler"
           type="button"
@@ -39,34 +40,35 @@ const Navbar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        {/* Enlaces de navegación */}
         <div className="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo02">
-          <ul className="navbar-nav d-flex gap-4">
-            <li className="nav-item">
-              <a className="nav-link" href="#catalogo">
-                <FaCar className="nav-icon" />
-                <span>Catálogo</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#about">
-                <FaHome className="nav-icon" />
-                <span>Sobre nosotros</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/simulador" target="_blank" rel="noopener noreferrer">
-                <FaCalculator className="nav-icon" />
-                <span>Simulador</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#contact">
-                <FaEnvelope className="nav-icon" />
-                <span>Contáctanos</span>
-              </a>
-            </li>
-          </ul>
+          {!isAutoDetalle && (
+            <ul className="navbar-nav d-flex gap-4">
+              <li className="nav-item">
+                <a className="nav-link" href="#catalogo">
+                  <FaCar className="nav-icon" />
+                  <span>Catálogo</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#about">
+                  <FaHome className="nav-icon" />
+                  <span>Sobre nosotros</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/simulador" target="_blank" rel="noopener noreferrer">
+                  <FaCalculator className="nav-icon" />
+                  <span>Simulador</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#contact">
+                  <FaEnvelope className="nav-icon" />
+                  <span>Contáctanos</span>
+                </a>
+              </li>
+            </ul>
+          )}
         </div>
       </div>
     </nav>
