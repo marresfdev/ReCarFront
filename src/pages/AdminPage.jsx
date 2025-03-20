99ccf92a6ad52fb926e97022f30ab736cf363948
marresfdev <marresfdev@gmail.com>
2025-03-19 18:03:24 -0600

    login design finished

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import Login from "../components/FormLogin"
-import "../styles/AdminPage.css"
+import Login from "../components/FormLogin";
+import "../styles/AdminPage.css";
+import logo from "../assets/recarlogo.png";
 
 const AdminPage = () => {
     return (
         <div className="admin-container">
-            <h2 className="admin-title">ERP ReCar Motors</h2>
+            <div className="title-container">
+                <img src={logo} alt="ReCar Motors Logo" className="admin-logo" />
+                <h2 className="admin-title">ERP ReCar Motors</h2>
+                <br />
+                <br />
+            </div>
             <Login />
         </div>
     );
 };
-
 
 export default AdminPage;
